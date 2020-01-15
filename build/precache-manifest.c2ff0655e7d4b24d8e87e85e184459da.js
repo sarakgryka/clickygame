00cf3a6b07e4be6a5715d6d986cdeab95ca2ad2b
Sara Gryka <sarakgryka@hotmail.com>
2020-01-14 20:00:39 -0700

    deploy

---
+++ build/precache-manifest.c2ff0655e7d4b24d8e87e85e184459da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c5420904e0fd127a8615b7bb9b528ec",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "d52f5efb6282e9a559c6",
+    "url": "/clickygame/static/css/main.b796d57c.chunk.css"
+  },
+  {
+    "revision": "f2838d9279c3aaed4c30",
+    "url": "/clickygame/static/js/2.d780e460.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/clickygame/static/js/2.d780e460.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d52f5efb6282e9a559c6",
+    "url": "/clickygame/static/js/main.a97fd011.chunk.js"
+  },
+  {
+    "revision": "89287fbab32696f15ef4",
+    "url": "/clickygame/static/js/runtime-main.b6e92f05.js"
+  }
+]);
